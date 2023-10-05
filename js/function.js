4af122c68496f9f8da84d8094aa7b23404b4e41c
R4GE <rageevgeniy@gmail.com>
2023-10-05 20:50:00 +0300

    Добавит дополнительную функцию

--- js/function.js
+++ js/function.js
@@ -3,10 +3,24 @@ console.log(lengthCheck('проверяемая строка', 18));
 
 const palindrome = function (string) {
   let comparisonString = '';
-  let processedString = string.replaceAll(' ','').toLowerCase();
+  const processedString = string.replaceAll(' ','').toLowerCase();
   for(let i = string.length - 1; i >= 0; i--) {
     comparisonString += processedString[i];
   }
   return  string === comparisonString;
-}
+};
 console.log(palindrome('топот'));
+
+function extractDigits(string) {
+  let result = '';
+
+  for(let i = 0; i < string.length; i++) {
+    const digit = parseInt(string[i], 10);
+
+    if (!Number.isNaN(digit) && digit <= 9 && digit >= 0) {
+      result += digit.toString();
+    }
+  }
+  return result === '' ? NaN : parseInt(result, 10);
+}
+console.log(extractDigits('а я томат'));
