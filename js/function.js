7ce40936053f7920d5f52d433d2bb66fe2827ddf
R4GE <rageevgeniy@gmail.com>
2023-10-06 17:47:48 +0300

    fix

--- js/function.js
+++ js/function.js
@@ -1,26 +1,27 @@
-const lengthCheck = (string, maxLength) => string.length <= maxLength;
-console.log(lengthCheck('проверяемая строка', 18));
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
+checkStringLength('проверяемая строка', 18);
 
-const palindrome = function (string) {
+const isPalindrome = (string) => {
   let comparisonString = '';
   const processedString = string.replaceAll(' ','').toLowerCase();
-  for(let i = string.length - 1; i >= 0; i--) {
+  for (let i = string.length - 1; i >= 0; i--) {
     comparisonString += processedString[i];
   }
-  return  string === comparisonString;
+  return string === comparisonString;
 };
-console.log(palindrome('топот'));
+isPalindrome('топот');
 
-function extractDigits(string) {
+const extractDigits = (string) => {
   let result = '';
 
-  for(let i = 0; i < string.length; i++) {
+  for (let i = 0; i < string.length; i++) {
     const digit = parseInt(string[i], 10);
 
     if (!Number.isNaN(digit) && digit <= 9 && digit >= 0) {
       result += digit.toString();
     }
   }
+
   return result === '' ? NaN : parseInt(result, 10);
-}
-console.log(extractDigits('а я томат'));
+};
+extractDigits('2023 год');
