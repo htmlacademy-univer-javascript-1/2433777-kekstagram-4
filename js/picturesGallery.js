726773697bf76d49ee809ba3be225b402777f1dd
ragelyoux <135124627+ragelyoux@users.noreply.github.com>
2023-12-15 16:56:01 +0300

    Update picturesGallery.js

--- js/picturesGallery.js
+++ js/picturesGallery.js
@@ -5,16 +5,26 @@ import { getPictures } from './data.js';
 const container = document.querySelector('.pictures');
 const photos = getPictures();
 
-const renderGallery = () =>{
-  container.addEventListener('click', (evt) =>{
+const renderGallery = () => {
+  if (!container) {
+    throw('Элемент контейнера не найден');
+  }
+
+  container.addEventListener('click', (evt) => {
     const thumbnail = evt.target.closest('[data-thumbnail-id]');
-    if(!thumbnail){
-      // throw 'Не существует такого элемента';
+    if (!thumbnail) {
+      throw ('Миниатюра не найдена');
     }
     evt.preventDefault();
     const picture = photos.find((item) => item.id === +thumbnail.dataset.thumbnailId);
-    showBigPicture(picture);
+    if (picture) {
+      showBigPicture(picture);
+    } else {
+      throw ('Картинка не найдена');
+    }
   });
+
   renderThumbnails(photos);
 };
-export{renderGallery};
+
+export { renderGallery };
